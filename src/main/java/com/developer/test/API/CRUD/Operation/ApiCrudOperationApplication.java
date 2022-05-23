07b30d8d3ea670c0b760ed8d0a26380c2f5fe90a
abdul.kamara_ubuntua <abdul.kamara@ubuntuafrika.com>
2022-05-23 01:25:23 +0000

    Update: Blank project

---
+++ src/main/java/com/developer/test/API/CRUD/Operation/ApiCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.developer.test.API.CRUD.Operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/test/API/CRUD/Operation/ApiCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.test.API.CRUD.Operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
